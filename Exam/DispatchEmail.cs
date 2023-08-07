113c444c34d3bbda836515980158b44bf6e94450
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-08-07 07:07:44 +0500

    Add project files.

---
+++ Exam/DispatchEmail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImtihonForFirstMonth
+{
+    public static class DispatchEmail
+    {
+        //ismiz
+        /*var name = Console.ReadLine();
+                Console.WriteLine("email");
+        var email = Console.ReadLine();
+                Console.WriteLine("enter passaword");
+        var pass = Console.ReadLine();
+                var Email = new EmailService(email, pass);
+                Email.SendEmail(email, name);*/
+
+        public static bool SendEmail(string receiverEmail, string name)
+        {
+            try
+            {
+                var CredentialAddress = "abdurahmonsadriddinov0412@gmail.com";
+                var CredentialPassword = "bypucbhumkeghlab";
+
+                var mail = new MailMessage(CredentialAddress, receiverEmail);
+                mail.Subject = "Siz muvaffaqiyatli registratsiyadan o'tdingiz";
+                mail.Body = "Hurmatli {{User}}, siz sistemadan muvaffaqiyatli o'tdingiz Tabriklaymiz".Replace("{{User}}", name);
+                var smtpClient = new SmtpClient("smtp.gmail.com", 587); // Replace with your SMTP server address and port
+                smtpClient.Credentials = new NetworkCredential(CredentialAddress, CredentialPassword);
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mail);
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+
+}
---
+++ Exam/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImtihonForFirstMonth
+{
+    public class User
+    {
+        //User modeli - foydalanuvchi ismi, familyasi, sharifi, email addresi, username va aktiv foydalanuvchi ekanligi haqidagi ma’lumotlarni saqlovchi model
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public bool IsActived { get; set; }
+
+        public User() { }
+
+        public User(string name, string firstName, string lastName, string email, string username)
+        {
+            Name = name;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Username = username;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name.PadLeft(20)}\nFirstName: {FirstName.PadLeft(20)}\n" + $"EmailAddress: {Email.PadLeft(30)}\nUserName: {Username.PadLeft(15)}\n\n";
+
+        }
+    }
+}
+
